1cb16792e3c019c94b7fbed04df197c7823018a3
zMr-Profesor4556 <nipunofficial4@gmail.com>
2025-08-09 00:33:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || " sjsnssjshhshrhrbbb", // ඔයාගෙ session id එක
-MONGODB: process.env.MONGODB || "mongodb://mongo:sjjkkd",  //ඔයාගෙ public mongoDb url එක
+SESSION_ID: process.env.SESSION_ID || "eVlGGIKb#JJYayGf0kgQAvMci5jPh7GzNIa8Ic0JHYkaVXEc2v2w", // ඔයාගෙ session id එක
+MONGODB: process.env.MONGODB || "mongodb+srv://dbuser002:nipun123n@cluster0.otnwzii.mongodb.net/ ",  //ඔයාගෙ mongoDb url එක
 };
- 
